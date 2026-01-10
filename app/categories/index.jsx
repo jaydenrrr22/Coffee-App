bca6f2f705a1a6558fdd06fdbbf400fe92884cb9
jaydenrrr22 <sryu@charlotte.edu>
2026-01-10 00:22:00 -0500

    minor change in home page

---
+++ app/categories/index.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const CategoryScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Testing</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+});
+
+export default CategoryScreen;
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,20 @@
 import PostItImage from "@/assets/images/Bluenolia.png";
-import { Image, StyleSheet, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const HomeScreen = () => {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
       <Image source={PostItImage} style={styles.image} />
-      {/* <Text style={styles.title}>Welcome to the Bluenolia</Text> */}
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/categories")}
+      >
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -29,6 +38,20 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginBottom: 10,
     color: "#333",
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "#ffffffff",
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    bottom: 225,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#000000ff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
 
